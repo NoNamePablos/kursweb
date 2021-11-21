04c9284cc8f208251e760c6aa9b9114713c68a29
Sergey <axuitebe13@mail.ru>
2021-11-21 19:38:18 +0300

    Верстка модального окна

--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,7 @@
 const btns = document.querySelectorAll('.btn');
 const modalOverlay = document.querySelector('.modal-overlay ');
 const modals = document.querySelectorAll('.modal');
+const modalClose=document.querySelector('.modal-close');
 
 btns.forEach((el) => {
 	el.addEventListener('click', (e) => {
@@ -18,7 +19,7 @@ btns.forEach((el) => {
 modalOverlay.addEventListener('click', (e) => {
 	console.log(e.target);
 
-	if (e.target == modalOverlay) {
+	if (e.target == modalOverlay||e.target==modalClose) {
 		modalOverlay.classList.remove('modal-overlay--visible');
 		modals.forEach((el) => {
 			el.classList.remove('modal--visible');
