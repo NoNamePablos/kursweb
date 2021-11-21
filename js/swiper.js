f660042e76cd2ff6fd24495d11cc359893339883
Sergey <axuitebe13@mail.ru>
2021-11-21 22:22:20 +0300

    Update

---
+++ js/swiper.js
@@ -0,0 +1,23 @@
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    autoplay: {
+        delay: 2000,
+      },
+      slidesPerView: 3,
+      spaceBetween: 10,
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+  });
+  
