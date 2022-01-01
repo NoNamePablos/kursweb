fb8a5a322361b3d52ae8736985bbb48b628b07fc
Sergey <axuitebe13@mail.ru>
2022-01-01 17:34:00 +0300

    изментил шапку чсайта

--- app/helpers/path.php
+++ app/helpers/path.php
@@ -2,6 +2,6 @@
 /*define('BASE_URL','');*/
 
 /*define('BASE_URL','http://localhost/kurs/kursweb/');*/
-define('BASE_URL','http://kursweb/');
+define('BASE_URL','http://kursweb1/');
 const SITE_ROOT=__DIR__;
 define("ROOT_PATH",realpath(dirname(__FILE__)));
